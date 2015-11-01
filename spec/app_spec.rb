aa33cc72d570b59dcf749a2fae6dd38f49c74d1c
rlmh <lmherrera86@gmail.com>
2015-11-02 00:41:42 +0800

    fix routes added tests but not working added vcr added webmock added logging there was a typo added helper for spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,8 +4,23 @@ require 'json'
 
 describe 'Getting the root of the service' do
   it 'Should return ok' do
-    get '/api/v1/'
+    get '/'
     last_response.must_be :ok?
     last_response.body.must_match(/queenshop/i)
   end
 end
+=begin
+describe 'Get item information' do
+	PASSED_TESTS.each_pair do |test, params|
+		it "must be identical to the file #{test}" do
+			VCR.use_cassette("#{test}") do
+				test_info = File.read("./spec/fixtures/#{test}")
+				header = { 'CONTENT_TYPE' => 'application/json' }
+    		body = params
+				post '/happy/', body.to_json, header
+					last_response.body.must_equal test_info
+			end
+		end
+  end
+end
+=end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
 ENV['RACK_ENV'] = 'test'
 
-require 'bundler/setup'
-require 'sinatra/base'
+require ::File.expand_path('../../config/environment', __FILE__)
 require 'minitest/autorun'
+require 'minitest/rg'
 require 'rack/test'
-require_relative '../app'
+require 'vcr'
+require 'bundler/setup'
+require 'webmock/minitest'
+require 'yaml'
+#require_relative '../app'
 
 include Rack::Test::Methods
+
+# params file => parameters hash
+PASSED_TESTS = {'page1'=> {pages: '1'}}
+FAIL_TEST = ['mmjjhii33.99023j9893'] # will do later to call random
 
 def app
   QueenShopApi::SinatraApp
@@ -17,3 +25,14 @@ def random_str(n)
   srand(n)
   (0..n).map { ('a'..'z').to_a[rand(26)] }.join
 end
+
+VCR.configure do |config|
+	config.cassette_library_dir = 'spec/fixtures/cassette'
+  config.hook_into :webmock
+end
+
+def load_yml(file_path)
+	YAML.load(File.read(file_path))
+end
+
+
