23c114209ab9af14fa6fa12dc7ec0c55a5dfde57
Even <kiki44552002@gmail.com>
2015-11-02 10:03:32 +0800

    make all tests passed

--- app.rb
+++ app.rb
@@ -11,23 +11,6 @@ class SinatraApp < Sinatra::Base
   # Standard Sinatra configurations
   configure :production, :development do
     enable :logging
-    get '/' do
-      'Queenshop is up and working. See documentation at its ' \
-        '<a href="https://github.com/hola2soa/QueenShopWebApi">' \
-        'Github repo - master branch</a>'
-    end
-# =begin		
-		post '/happy' do
-			content_type :json
-    	begin
-      	req = JSON.parse(request.body.read)
-    	rescue => e
-      	logger.error "Error: #{e.message}"
-      	halt 400
-    	end
-    	check_items(req['items'], req['prices'], req['pages']).to_json
-		end
-# =end
   end
 
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,6 +9,51 @@ describe 'Getting the root of the service' do
     last_response.body.must_match(/queenshop/i)
   end
 end
+
+describe 'Getting cadet information' do
+  it 'should return their prices' do
+    get '/api/v1/qs/10.json'
+    last_response.must_be :ok?
+  end
+
+  it 'should return 404 for unknown items' do
+    get "/api/v1/check/#{random_str(20)}.json"
+    last_response.must_be :not_found?
+  end
+end
+
+describe 'Checking items for prices' do
+  it 'should find matching prices' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = {
+      items: ['M', 'S'],
+      prices: ['290','250'],
+      pages: '1..5'
+    }
+
+    post '/api/v1/check', body.to_json, header
+    last_response.must_be :ok?
+  end
+  it 'should return 404 for unknown items' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = {
+      items: [random_str(15), random_str(15)],
+      prices: ['800']
+    }
+
+    post '/api/v1/check', body.to_json, header
+    last_response.must_be :not_found?
+  end
+
+  it 'should return 400 for bad JSON formatting' do
+    header = { 'CONTENT_TYPE' => 'application/json' }
+    body = random_str(50)
+
+    post '/api/v1/check', body, header
+    last_response.must_be :bad_request?
+  end
+end
+
 =begin
 describe 'Get item information' do
 	PASSED_TESTS.each_pair do |test, params|
