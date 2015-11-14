47714bacc433e0e1bfc3bfc45eef118abac43b74
frank1234211 <frank1234211@gmail.com>
2015-11-15 02:13:10 +0800

    try to fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -147,7 +147,80 @@ class SinatraApp < Sinatra::Base
 	delete '/query/:id', &delete_query_id
 
 
-	
+	   #Web API
+		root = lambda do
+		  'Hello,Queenshop is up and working.  Please see documentation at its ' \
+		  '<a href="https://github.com/hola2soa/QueenShopWebApi">' \
+		  'Github repo - master branch</a>'
+		end
+
+        show = lambda do
+          content_type :json
+          get_items(params[:item]).to_json
+        end
+		
+		
+        post_query = lambda do
+          content_type :json
+          begin
+            req = JSON.parse(request.body.read)
+            logger.info req
+          rescue => e
+            logger.error "Error: #{e.message}"
+            halt 400
+          end
+
+          request = Request.new(
+            items: req['items'],
+            prices: req['prices'],
+            pages: req['pages']
+          )
+
+          if request.save
+            status 201
+            redirect "/api/v1/queenshop/query/#{request.id}", 303
+          else
+            logger.error 'Error saving request to database'
+            halt 500, 'Error saving request request to the database'
+          end
+        end
+
+		
+        get_query = lambda do
+          content_type :json
+          begin
+            request = Request.find(params[:id])
+            items = JSON.parse(request.items)
+            prices = JSON.parse(request.prices)
+            pages = request.pages
+          rescue
+            logger.error 'Error while fetching request from database'
+            halt 400
+          end
+
+          begin
+            check_items(items, prices, pages).to_json
+          rescue
+            logger.error 'Lookup of Queenshop failed'
+            halt 500, 'Lookup of Queenshop failed'
+          end
+
+          { id: request.id, items: items,
+            prices: prices, pages: pages
+          }.to_json
+        end
+		
+		delete_query = lambda do
+			request = Request.destroy(params[:id])
+			status(request > 0 ? 200 : 404)
+		end
+		
+		
+		get '/api/v1/', &root
+        get '/api/v1/:item', &show
+        get '/api/v1/query/:id', &get_query
+        post '/api/v1/query', &post_query
+	    delete '/api/v1/query/:id', &delete_query
 	
 	
 	  
