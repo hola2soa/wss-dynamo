5696812bd3538d7db288a2f114a58316e378716c
frank1234211 <frank1234211@gmail.com>
2015-12-14 09:12:25 +0800

    Update database.rb

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,9 @@
 require 'dynamoid'
-
+ConfigEnv.path_to_config("#{__dir__}/config_env.rb")
+Aws.config.update({
+    region: 'us-west-2',
+    credentials: Aws::Credentials.new('REPLACE_WITH_ACCESS_KEY_ID', 'REPLACE_WITH_SECRET_ACCESS_KEY')
+})
 Dynamoid.configure do |config|
   config.adapter = 'aws_sdk_v2'
   config.namespace = 'wss'
